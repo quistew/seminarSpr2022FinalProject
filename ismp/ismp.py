b5459a930bcc325657d82498e73231236eaa9c22
quistew <quistew@gmail.com>
2022-05-02 08:18:00 -0600

    update ismp script

--- ismp/ismp.py
+++ ismp/ismp.py
@@ -1,16 +1,17 @@
 import numpy as np
 from sklearn.manifold import Isomap
-
-from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
 
 def main():
     X_train = np.loadtxt("../data/training.txt")
-    # X_test = np.loadtxt("../data/test.txt")
-    # X_train, _ = load_digits(return_X_y=True)
+    X_test = np.loadtxt("../data/test.txt")
 
     embedding = Isomap(n_neighbors=5, n_components=2)
-    X_transformed = embedding.fit_transform(X_train[:,0:3])
-    print(X_transformed.shape)
+    X_train_transformed = embedding.fit_transform(X_train[:, 0:3])
+    X_test_transformed = embedding.fit_transform(X_test[:, 0:3])
+    print(X_train_transformed.shape)
+
+
 
 
 main()
